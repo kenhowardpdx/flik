e88679ff1105545da524112e12a2ce8306df4fe3
Ken Howard <ken@simplicatedweb.com>
2014-05-21 14:33:22 -0700

    fixed app

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
+var logfmt = require('logfmt');
+var gzippo = require('gzippo');
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.use(logfmt.requestLogger());
+
+app.use(gzippo.staticGzip(__dirname + '/dist'));
+
+var port = Number(process.env.PORT || 5000);
+
+app.listen(port, function() {
+  console.log("Listening on port " + port);
+});
