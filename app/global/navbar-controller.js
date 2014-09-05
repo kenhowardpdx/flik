d1c343d3ebb0a00254d74f012f6771042c284688
Ken Howard <ken@simplicatedweb.com>
2014-09-05 12:49:33 -0700

    Adjusted isActive to use regex matching

--- app/global/navbar-controller.js
+++ app/global/navbar-controller.js
@@ -2,8 +2,15 @@
 
 angular.module('app')
     .controller('NavCtrl', function ($scope, $location) {
-        $scope.isActive = function(route) {
-            return route === $location.path();
+
+        $scope.isActive = function (r) {
+            var routes = r.join('|'),
+                regexStr = '^\/(' + routes + ')',
+                path = new RegExp(regexStr);
+            if(r[0] === 'home' && $location.path() === '/') {
+                return true;
+            }
+            return path.test($location.path());
         };
 
   		$scope.isCollapsed = true;
