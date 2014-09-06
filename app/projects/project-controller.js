be6aabf4db8329267b9c09fd5f1b866cdc8c5595
Ken Howard <ken@simplicatedweb.com>
2014-09-06 09:03:08 -0700

    JSHINT error fixes

--- app/projects/project-controller.js
+++ app/projects/project-controller.js
@@ -1,51 +1,52 @@
 (function(){
+	'use strict';
 
-angular.module('app')
-	.controller('ProjectCtrl', ['$scope','$location','$routeParams','$http','toaster','CONFIG', function ($scope, $location, $routeParams, $http, toaster, CONFIG) {
-	// Do awesome things
+	angular.module('app')
+		.controller('ProjectCtrl', ['$scope','$location','$routeParams','$http','toaster','CONFIG', function ($scope, $location, $routeParams, $http, toaster, CONFIG) {
+		// Do awesome things
 
-	$scope.projectId = $routeParams.projectId;
+		$scope.projectId = $routeParams.projectId;
 
-	if($scope.projectId) {
-		// Get record
-		$http.get(CONFIG.API_URL + 'api/projects/' + $scope.projectId)
-		.success(function(data) {
-			$scope.project = data;
-		})
-		.error(function(data) {
-			toaster.pop('error', 'Something went horribly wrong');
-		});
-	} else {
-		// Create empty record
-		$scope.project = {};
-	}
-
-	$scope.saveRecord = function() {
-		var data = $scope.project;
-		var id = $scope.projectId;
-		if (id) {
-			// Update record
-			$http.put(CONFIG.API_URL + 'api/projects/' + id, data)
+		if($scope.projectId) {
+			// Get record
+			$http.get(CONFIG.API_URL + 'api/projects/' + $scope.projectId)
 			.success(function(data) {
-				toaster.pop('success','Updated Project');
-				$location.url('/projects');
+				$scope.project = data;
 			})
-			.error(function(status) {
+			.error(function() {
 				toaster.pop('error', 'Something went horribly wrong');
 			});
 		} else {
-			// Add record
-			$http.post(CONFIG.API_URL + 'api/projects', data)
-			.success(function(data){
-				toaster.pop('success','Created Project');
-				$location.url('/projects');
-			})
-			.error(function(status){
-				toaster.pop('error', 'Something went horribly wrong');
-			});
+			// Create empty record
+			$scope.project = {};
 		}
-	};
+
+		$scope.saveRecord = function() {
+			var data = $scope.project;
+			var id = $scope.projectId;
+			if (id) {
+				// Update record
+				$http.put(CONFIG.API_URL + 'api/projects/' + id, data)
+				.success(function() {
+					toaster.pop('success','Updated Project');
+					$location.url('/projects');
+				})
+				.error(function() {
+					toaster.pop('error', 'Something went horribly wrong');
+				});
+			} else {
+				// Add record
+				$http.post(CONFIG.API_URL + 'api/projects', data)
+				.success(function(){
+					toaster.pop('success','Created Project');
+					$location.url('/projects');
+				})
+				.error(function(){
+					toaster.pop('error', 'Something went horribly wrong');
+				});
+			}
+		};
 
 
-	}]);
+		}]);
 })();
