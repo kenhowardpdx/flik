57c4a37d91d29a609cfa76aca252833931d8e820
Ken Howard <ken@simplicatedweb.com>
2014-09-08 12:58:02 -0700

    fix formatting. Added debug flag

--- app/config.js
+++ app/config.js
@@ -5,15 +5,16 @@
 
     var configData = {
         'CONFIG': {
+            'DEBUG': true,
             'SITE_NAME': 'Citrus',
             'APP_VERSION': '1.0.0',
             'API_URL': 'https://csgprohackathonapi.azurewebsites.net/',
             'USERNAME_PREFIX': 'c*'
-    }
+        }
     };
     angular.forEach(configData, function(key,value) {
-    angular.module('site-config').constant(value,key);
-    // Load config constants
+        angular.module('site-config').constant(value,key);
+        // Load config constants
     });
 
     angular.module('app')
