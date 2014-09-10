ed6a4df71dcfe89a0a14b6a2fb11177bd275fe28
Ken Howard <ken@simplicatedweb.com>
2014-09-09 23:24:12 -0700

    Set up basic comment directive that handles twitter style mentions and hash tags.

---
+++ app/directives/comment-directive.js
@@ -0,0 +1,54 @@
+(function() {
+	'use strict';
+
+	angular.module('app')
+		.directive('comment', ['$timeout', function($timeout) {
+				return {
+					restrict: 'AEC',
+					scope: {
+						hashitems: '=',
+						atitems: '=',
+						prompt: '@',
+						title: '@',
+						model: '=',
+						selectedHashItems: '=',
+						selectedAtItems: '='
+					},
+					link: function(scope) {
+						scope.handleSelection = function(type,item) {
+							var symbol = '',
+								pattern;
+							if(type === 'hash') {
+								scope.selectedHashItems.push(item);
+								scope.selectedHashItem = true;
+								symbol = '#';
+							} else {
+								scope.selectedAtItems.push(item);
+								scope.selectedAtItem = true;
+								symbol = '@';
+							}
+							pattern = new RegExp(symbol + '([a-zA-Z0-9]*)');
+							scope.model = scope.model.replace(pattern, symbol + item.Name);
+						};
+						scope.current = 0;
+						scope.selectedHashItem = true;
+						scope.selectedAtItem = true; // hides the list initially
+						scope.isSelected = function() {
+							return false;
+						};
+						scope.evaluateKey = function($event) {
+							var keyCode = $event.keyCode;
+							if (keyCode === 51) // #
+							{
+								scope.selectedHashItem = false;
+							}
+							if (keyCode === 50) // @
+							{
+								scope.selectedAtItem = false;
+							}
+						}
+					},
+					templateUrl: 'partials/comment.html'
+				};
+			}]);
+})();
