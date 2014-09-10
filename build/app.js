499e2fd91a555fa211bec138211c75666320e7d2
Ken Howard <ken@simplicatedweb.com>
2014-09-09 23:25:20 -0700

    latest build with twitter style comments on time entry.

--- build/app.js
+++ build/app.js
@@ -128,6 +128,61 @@
 
 (function() {
 	'use strict';
+
+	angular.module('app')
+		.directive('comment', ['$timeout', function($timeout) {
+				return {
+					restrict: 'AEC',
+					scope: {
+						hashitems: '=',
+						atitems: '=',
+						prompt: '@',
+						title: '@',
+						model: '=',
+						selectedHashItems: '=',
+						selectedAtItems: '='
+					},
+					link: function(scope) {
+						scope.handleSelection = function(type,item) {
+							var symbol = '',
+								pattern;
+							if(type === 'hash') {
+								scope.selectedHashItems.push(item);
+								scope.selectedHashItem = true;
+								symbol = '#';
+							} else {
+								scope.selectedAtItems.push(item);
+								scope.selectedAtItem = true;
+								symbol = '@';
+							}
+							pattern = new RegExp(symbol + '([a-zA-Z0-9]*)');
+							scope.model = scope.model.replace(pattern, symbol + item.Name);
+						};
+						scope.current = 0;
+						scope.selectedHashItem = true;
+						scope.selectedAtItem = true; // hides the list initially
+						scope.isSelected = function() {
+							return false;
+						};
+						scope.evaluateKey = function($event) {
+							var keyCode = $event.keyCode;
+							if (keyCode === 51) // #
+							{
+								scope.selectedHashItem = false;
+							}
+							if (keyCode === 50) // @
+							{
+								scope.selectedAtItem = false;
+							}
+						}
+					},
+					templateUrl: 'partials/comment.html'
+				};
+			}]);
+})();
+
+(function() {
+	'use strict';
 	angular.module('app')
 		.filter('aDate', function($filter) {
 		  var suffixes = ["th", "st", "nd", "rd"];
@@ -651,6 +706,20 @@
 			httpService.getCollection('projects').then(function(projects) {
 				$scope.availableProjects = projects;
 			});
+
+			$scope.availableContexts = [
+				{
+					ContextId: 0,
+					Name: 'home'
+				},
+				{
+					ContextId: 0,
+					Name: 'office'
+				}
+			];
+
+			$scope.selectedProjects = [];
+			$scope.selectedContexts = [];
 
 			if(id) {
 				httpService.getItem('timeentries',$scope.entryId).then(function(entry) {
