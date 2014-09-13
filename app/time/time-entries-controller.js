e24bb2f529836f5d858e8e9f89823e01c7ea6d8e
Ken Howard <ken@simplicatedweb.com>
2014-09-13 11:47:04 -0700

    Added function to generate color class

--- app/time/time-entries-controller.js
+++ app/time/time-entries-controller.js
@@ -34,6 +34,7 @@
                 var dateStr = '' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear();
                 httpService.getCollection('timeentries/date/' + dateStr).then(function(entries) {
                     $scope.timeEntries = entries;
+                    getColorClassForEntries($scope.timeEntries);
                 });
             };
 
@@ -41,6 +42,26 @@
                 var date = $scope.timeEntryDate;
                 var dateStr = '' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear();
                 $location.url('/time/edit/' + dateStr + '/' + entry.TimeEntryId);
+            };
+
+            var getColorClassForEntries = function (entries) {
+                for(var i = 0; i < entries.length; i++) {
+                    entries[i].ProjectColorClass = getColorClass(entries[i].ProjectRoleId,entries[i].ProjectTaskId);
+                }
+            };
+
+            var getColorClass = function (num1,num2) {
+                var colorClass = 'project-color-';
+                var num = 0;
+                var num1 = num1.toString().slice(-1);
+                var num2 = num2.toString().slice(-1);
+                if(num1 > num2) {
+                    num = num2 / num1;
+                } else {
+                    num = num1 / num2;
+                }
+                colorClass = colorClass + Math.floor(num * 10);
+                return colorClass;
             };
 
             getTimeEntries();
